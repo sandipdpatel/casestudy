378d88c2e27b4f040e90bc8dd4282ca8970a22b5
Sandip Patel <sandip.patel@oracle.com>
2016-05-28 22:34:04 +0530

    adding db connection and other startup code

--- start.js
+++ start.js
@@ -13,15 +13,43 @@ function rest(){
 
 rest.prototype.connectMysql = function() {
     var self = this;
+    var pool      =    mysql.createPool({
+        connectionLimit : 100,
+        host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+		port	 : process.env.OPENSHIFT_MYSQL_DB_PORT,
+        user     : 'admin2cPSTM8',
+        password : 'SdUxGSv6Y4yq',
+        database : 'restful',
+        debug    :  false
+    });
+    pool.getConnection(function(err,connection){
+        if(err) {
+          self.stop(err);
+        } else {
+          self.configureExpress(connection);
+        }
+    });
 }
 
 rest.prototype.configureExpress = function(connection) {
+      var self = this;
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      app.use('/api', router);
+      var rest_router = new rest(router,connection,md5);
+      self.startServer();
 }
 
 rest.prototype.startServer = function() {
+      app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost',function(){
+          console.log("All right ! I am alive at Port 3000.");
+      });
 }
 
 rest.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL \n" + err);
+    process.exit(1);
 }
 
 new rest();
